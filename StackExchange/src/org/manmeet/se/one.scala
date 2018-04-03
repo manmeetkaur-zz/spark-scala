51ac11b3ea64ccf7f222faea4fc01d361ca84626
manmeetkaur <manmeetkaur.rangoola@gmail.com>
2018-04-02 21:14:01 -0500

    one issue sorted out

--- StackExchange/src/org/manmeet/se/one.scala
+++ StackExchange/src/org/manmeet/se/one.scala
@@ -4,27 +4,29 @@ import scala.xml.XML
 
 import org.apache.spark.sql.SparkSession
 
+/*
+ * Program to count the number of questions in the dataset and collect question ids
+ * 
+ */
 object one {
   
   def main(args: Array[String]) {
-//    if (args.length < 2) {
-//      System.err.println("Usage: two <input-file> <output-file>")
-//      System.exit(1)
-//    }
     
     val spark = SparkSession.builder().appName("Question 2").getOrCreate()
-    val data = spark.read.textFile("file:///Users/manmeet/Downloads/Posts.xml").rdd
+    val data = spark.read.textFile("file:///Users/manmeetkaur/Downloads/Posts.xml").rdd
     
     val ques = data
                 .filter(line => line.trim().startsWith("<row"))
                 .filter(line => line.trim().contains("PostTypeId=\"1\""))
     
-    val ques_ids = ques.map(line => {
-      val xml = XML.loadString(line)
-      xml.attribute("PostTypeId")
-    }
+    val ques_ids = ques.flatMap(line => {
+      val xml = XML.loadString(line.trim())
+      xml.attribute("Id")
+    })
     
     print(ques.count())
+    
+    ques_ids.foreach(print)
     
     spark.stop()
   }
---
+++ StackExchange/src/org/manmeet/se/two.scala
@@ -0,0 +1,33 @@
+package org.manmeet.se
+
+import scala.xml.XML
+
+import org.apache.spark.sql.SparkSession
+
+/*
+ * Monthly questions count –provide the distribution of number of questions asked per month
+ * 
+ */
+
+object two {
+  
+  def main(args: Array[String]) {
+    
+    val spark = SparkSession.builder().appName("Question 2").getOrCreate()
+    val data = spark.read.textFile("file:///Users/manmeet/Downloads/Posts.xml").rdd
+    
+    val ques = data
+                .filter(line => (line.trim().startsWith("<row")))
+                .filter(line => line.trim().contains("PostTypeId=\"1\""))
+                .flatMap(line => {
+                  val xml = XML.loadString(line)
+                  xml.attribute("CreationDate")
+                })
+    
+    val result = ques.map(line => (line.toString.substring(0,7), 1)).reduceByKey(_ + _)
+    
+    result.foreach(println)
+    
+    spark.stop()
+  }
+}
